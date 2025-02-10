1c0e1e50511c7d4abbac281545f104c860a70782
Trevor Kozek <tkozek@students.cs.ubc.ca>
2025-02-09 22:42:39 -0800

    Added specifications for Board.java

--- src/main/model/Board.java
+++ src/main/model/Board.java
@@ -1,5 +1,35 @@
 package model;
 
+// Represents a Scrabble Game Board
 public class Board {
+
+    // Initializes a new Board, with standard
+    // placement of Double word, Triple Word,
+    // Double Letter, Triple Letter Tiles
+
+    public Board() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Places special tiles at appropriate positions on board
+    private void initializeBoard() {
+
+    }
+    // REQUIRES: row < BOARD_HEIGHT and col < BOARD_WIDTH
+    //            and both row and col >= 0
+    // EFFECTS: returns true if and only if 
+    // all tiles in the desired section are BoardTiles
+    public boolean isSectionOccupied() {
+        return false;
+    }
+
+    // REQUIRES: 0 <= row < BOARD_HEIGHT and 0 <= col < BOARD_WIDTH
+    // EFFECTS: returns true if board[row][col] is a BoardTile
+    //    returns false if board[row][col] is a LetterTile
+    public boolean isTileOccupied() {
+        return false;
+    }
+
 
 }
