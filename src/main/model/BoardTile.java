7927a7e181b2ff704e7ea34b3dde133656eaec12
Trevor Kozek <tkozek@students.cs.ubc.ca>
2025-02-09 20:45:57 -0800

    Added class description for BoardTile and specification for LetterTile

--- src/main/model/BoardTile.java
+++ src/main/model/BoardTile.java
@@ -1,5 +1,15 @@
 package model;
 
+//Represents a space on the Scrabble board which
+// has not been played on yet. May have special properties
+// such as Triple Word, Double Word, Triple Letter, Double Letter
+// which impact scoring
 public class BoardTile implements Tile {
 
+
+    // Makes BoardTile with specified special tile properties
+    
+    public BoardTile() {
+
+    }
 }
--- src/main/model/LetterTile.java
+++ src/main/model/LetterTile.java
@@ -1,5 +1,24 @@
 package model;
 
+//Represents the playable tiles in a Scrabble Game
+
 public class LetterTile implements Tile {
 
+    //Creates a new tile of specified letter and point value
+    public LetterTile() {
+        
+    }
+
+
+    public char getCharacter() {
+        return '_';
+    }
+    public int getLetterPoints() {
+        return 0;
+    }
+
+    //EFFECTS: casts tile's character as a String type
+    public String getCharacterAsString() {
+        return "";
+    }
 }
