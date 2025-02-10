9098a78f06c0116543eef8ec8018f9358dcdd2f9
Trevor Kozek <tkozek@students.cs.ubc.ca>
2025-02-09 19:37:31 -0800

    Added ScrabbleApp.java, which contains main method and specified this class

--- src/main/model/Sample.java
+++ src/main/model/Sample.java
@@ -1,9 +1,5 @@
 package model;
 
 public class Sample {
-    int other;
 
-    public Sample() {
-        other = 4;
-    }
 }
---
+++ src/main/ui/ScrabbleApp.java
@@ -0,0 +1,17 @@
+package ui;
+// Represents a game of Scrabble
+
+public class ScrabbleApp {
+
+
+    //EFFECTS: Creates desired number of players and assigns
+    //       them each to a common board
+    public ScrabbleApp() {
+
+    }
+
+    // Play the game
+    public static void main(String[] args) {
+        
+    }
+}
