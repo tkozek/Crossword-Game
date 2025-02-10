55ca80644ddefdf68b0eb9c340313131a1e70650
Trevor Kozek <tkozek@students.cs.ubc.ca>
2025-02-09 21:06:01 -0800

    Added specification for TileBag.java

--- src/main/model/TileBag.java
+++ src/main/model/TileBag.java
@@ -1,8 +1,36 @@
 package model;
 
+// Represents the draw bag of Letter Tiles in 
+// a Scrabble Game
 public class TileBag {
     
+    //Initializes tile bag with number of each 
+    //letter standard to a Scrabble Game
     public TileBag() {
 
+    
     }
+
+    //REQUIRES: Tile Bag is empty
+    //MODIFIES: this
+    //EFFECTS: Adds correct number of each letter tile
+    // and blank tile to the tile bag.
+    private void initializeTiles() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: Adds a new Letter Tile to the tile bag
+    //          the specified number of times
+    private void addTiles() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: Removes random tile from tile bag and returns it,
+    //            null if empty
+    public LetterTile drawTile() {
+        return new LetterTile();
+    }
+
 }
