73bf0dbac8c80ef09f9735990850410e242ef166
Trevor Kozek <tkozek@students.cs.ubc.ca>
2025-02-15 23:23:14 -0800

    Implemented Move.java which involved adding a few more setter and getter methods. Added more tests for Move.java

--- src/main/model/Move.java
+++ src/main/model/Move.java
@@ -5,47 +5,98 @@ import java.util.ArrayList;
 
 // A move made by a player, either a word played or a swap
 public class Move {
-    
+    private MoveType moveType;
+    private int pointsForMove;
+    private List<LetterTile> lettersInvolved;
+    private Direction direction;
+    private int startRow;
+    private int startCol;
+    private Board board;
+    private Player player;
+
     // Constructor if the player played a word on a board
-    public Move(Player p, Board board, List<LetterTile> letters, int startRow, int startCol, int pointsEarned, Direction direction) {
+    public Move(Player player, Board board, List<LetterTile> letters, 
+    int startRow, int startCol, int pointsEarned, Direction direction) {
+        this.moveType = MoveType.PLAY_WORD;
+        this.player = player;
+        this.board = board;
+        this.pointsForMove = pointsEarned;
+        this.lettersInvolved = letters;
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.direction = direction;
 
     }
 
     // Constructor if the player swapped letters
 
-    public Move(Player p, Board b, List<LetterTile> swappedLetters, List<LetterTile> receivedLetters) {
+    public Move(Player player, Board board, List<LetterTile> swappedLetters, List<LetterTile> receivedLetters) {
+        this.moveType = MoveType.SWAP_TILES;
+        this.player = player;
+        this.board = board;
+        swappedLetters.addAll(receivedLetters);
+        this.lettersInvolved = swappedLetters;
+        this.pointsForMove = 0;
 
     }
 
     // Constructor for end of game where last player gains points from unplayed tiles on opponent racks
     // REQUIRES: Exchanged points >= 0;
-    public Move(Player player, Board b, boolean wasLastToPlay, List<LetterTile> unplayedLetters, int exchangedPoints) {
+    public Move(Player player, Board board, boolean wasLastToPlay, List<LetterTile> unplayedLetters, int exchangedPoints) {
+        if (wasLastToPlay) {
+            this.moveType = MoveType.END_GAME_WINNER;
+            this.pointsForMove = exchangedPoints;
+        } else {
+            this.moveType = MoveType.END_GAME_LOSER;
+            this.pointsForMove = -1 * exchangedPoints;
+        }
+        this.player = player;
+        this.board = board;
+        this.lettersInvolved = unplayedLetters;
+        
 
     }
-    
+    public Player getPlayer() {
+        return this.player;
+    }
     public MoveType getMoveType() {
-        return MoveType.PLAY_WORD;
+        return this.moveType;
     }
     // EFFECTS: returns points gained OR lost
     // in a move, 0 for a swap
     public int getPointsForMove() {
-        return 0;
+        return this.pointsForMove;
     }
     // EFFECTS: returns list of letters played, swapped,
     //  or involved in point adjustments at end game
     public List<LetterTile> getLettersInvolved() {
-        return null;
+        return this.lettersInvolved;
+    }
+
+    public Board getBoard() {
+        return this.board;
     }
 
     // REQUIRES: getMoveType() == MoveType.PLAY_WORD
     public Direction getDirection() {
-        return Direction.DOWN;
+        return this.direction;
+    }
+    public int getStartRow() {
+        return this.startRow;
+    }
+    public int getStartColumn() {
+        return this.startCol;
     }
     // REQUIRES: getMoveType() == MoveType.PLAY_WORD, letter is 
     // uppercase between 'A' to 'Z' or '-'
     // EFFECTS: returns true if at least one 
     // letter in the move matches the given letter.
     public boolean moveContainsLetter(char letter) {
+        for (LetterTile letterTile : this.getLettersInvolved()) {
+            if (letterTile.getCharacter() == letter) {
+                return true;
+            }
+        }
         return false;
     }
 
--- src/test/model/MoveTest.java
+++ src/test/model/MoveTest.java
@@ -39,9 +39,13 @@ public class MoveTest {
     void testConstructorForPlayedWord() {
         playWord = new Move(testPlayer, board, p1Letters, 7,7, 10, Direction.DOWN);
         assertEquals(playWord.getMoveType(), MoveType.PLAY_WORD);
+        assertEquals(playWord.getPlayer(), testPlayer);
         assertEquals(playWord.getPointsForMove(), 10);
         assertEquals(playWord.getLettersInvolved(), p1Letters);
         assertEquals(playWord.getDirection(),Direction.DOWN); 
+        assertEquals(playWord.getStartColumn(), 7);
+        assertEquals(playWord.getStartRow(), 7);
+        assertEquals(playWord.getBoard(),board);
 
     }
     @Test
@@ -68,18 +72,23 @@ public class MoveTest {
     @Test
     void testConstructorForSwappedWord() {
         swap = new Move(testPlayer2, board, p1Letters, p2Letters);
+        assertEquals(swap.getPlayer(), testPlayer2);
         assertEquals(swap.getMoveType(), MoveType.SWAP_TILES);
         assertEquals(swap.getPointsForMove(), 0);
         p1Letters.addAll(p2Letters);
         assertEquals(swap.getLettersInvolved(),p1Letters);
+        assertEquals(swap.getBoard(),board);
     }
     @Test
     void testConstructorForEndGameAdjustment() {
         endGameLoser = new Move(testPlayer, board, false, p1Letters, 7);
+        assertEquals(endGameLoser.getPlayer(), testPlayer);
         assertEquals(endGameLoser.getMoveType(), MoveType.END_GAME_LOSER);
-
+        assertEquals(endGameLoser.getBoard(),board);
         endGameWinner = new Move(testPlayer2, board, true, p1Letters, 7);
+        assertEquals(endGameWinner.getPlayer(), testPlayer2);
         assertEquals(endGameWinner.getMoveType(),MoveType.END_GAME_WINNER);
+        assertEquals(endGameWinner.getBoard(),board);
     }
     
 }
