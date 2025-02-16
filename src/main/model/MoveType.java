3c51e74b69f2d0807d1ca8946f3fc20450decede
Trevor Kozek <tkozek@students.cs.ubc.ca>
2025-02-15 21:37:18 -0800

    Added MoveType enumeration to categorize moves into played word, swapped tiles, or end game adjustments

---
+++ src/main/model/MoveType.java
@@ -0,0 +1,8 @@
+package model;
+// Represents the type of move performed. A move can either be
+// playing a word, swapping between 0 and Player.MAX_NUM_TILES, 
+// or subtracting unplayed letters from score at the end of the 
+// game
+public enum MoveType {
+ PLAY_WORD, SWAP_TILES, END_GAME_WINNER, END_GAME_LOSER
+}
