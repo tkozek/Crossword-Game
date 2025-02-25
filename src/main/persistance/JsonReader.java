6b1398be456a44061ba0c6f23e5bd487103ce146
tkozek <tkozek@students.cs.ubc.ca>
2025-02-25 13:52:35 -0800

    Added specification for JsonReader.java

--- src/main/persistance/JsonReader.java
+++ src/main/persistance/JsonReader.java
@@ -1,13 +1,97 @@
 package persistance;
 
+import java.io.IOException;
+
 import org.json.JSONObject;
 
-public class JsonReader implements Writable {
+import model.ScrabbleGame;
 
-    @Override
-    public JSONObject toJSON() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toJSON'");
+public class JsonReader {
+
+    private String source;
+
+    // EFFECTS: constructs reader that reads 
+    // from data stored in file at source
+    public JsonReader(String source) {
+
     }
+
+    // EFFECTS: reads Scrabble Game from file
+    // and returns it; throws IOException if an
+    // error occurs reading data from file
+    public ScrabbleGame read() throws IOException {
+        return null;
+    }
+
+    // EFFECTS: reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+        return "";
+    }
+
+    // EFFECTS: parses ScrabbleGame from JSON Object and returns it
+    private ScrabbleGame parseGame(JSONObject jsonObject) {
+        return null;
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses board state from JSON object and adds it to 
+    // the Scrabble Game
+    private void addBoard(ScrabbleGame game, JSONObject jsonObject) {
+
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses board space (LetterTile or BoardTile) from JSON object and adds it to 
+    // the Scrabble Game
+    private void addBoardSpace(ScrabbleGame game, JSONObject jsonObject) {
+
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses tile bag from JSON object and adds it to 
+    // the Scrabble Game
+    private void addTileBag(ScrabbleGame game, JSONObject jsonObject) {
+        
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses players (LetterTile or BoardTile) from JSON object and adds it to 
+    // the Scrabble Game
+    private void addPlayers(ScrabbleGame game, JSONObject jsonObject) {
+        
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses player (LetterTile or BoardTile) from JSON object and adds it to 
+    // the Scrabble Game
+    private void addPlayer(ScrabbleGame game, JSONObject jsonObject) {
+        
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses full game history (all player histories combined)
+    // from JSON object and adds it to the Scrabble Game
+    private void addGameHistory(ScrabbleGame game, JSONObject jsonObject) {
+        
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses a player's history from JSON object and adds it to 
+    // the Scrabble Game
+    private void addPlayerHistory(ScrabbleGame game, JSONObject jsonObject) {
+        
+    }
+
+    // MODIFIES: game
+    // EFFECTS: parses player tiles from JSON object and adds it to 
+    // the player in the Scrabble Game
+    private void addPlayerTiles(ScrabbleGame game, JSONObject jsonObject) {
+        
+    }
+
+
+
+
+
 
 }
