1fa79afbe3e2d0b140e01aa36b0d715510c39acd
tkozek <tkozek@students.cs.ubc.ca>
2025-02-25 14:17:13 -0800

    Added tests for ScrabbleGame which don't involve JSON

---
+++ src/test/model/ScrabbleGameTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScrabbleGameTest {
+
+    private ScrabbleGame game;
+    private Board board;
+    private TileBag tileBag;
+    private Player player;
+
+
+    @BeforeEach
+    void setup() {
+        board = new Board();
+        tileBag = new TileBag();
+        game = new ScrabbleGame("test", board, tileBag);
+        player = new Player("player", board, tileBag);
+
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("test", game.getName());
+        game.addPlayer(player);
+        assertEquals(game.getNumPlayers(), 1);
+        assertEquals(game.getPlayers().get(0), player);
+        assertTrue(game.getHistory().getMoves().isEmpty());        
+    }
+}
