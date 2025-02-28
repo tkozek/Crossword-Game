6bb24258b9927d58a47341d732c560796761c9a7
Trevor Kozek <tkozek@students.cs.ubc.ca>
2025-02-27 22:02:54 -0800

    Added implementation for JsonWriter.java

--- src/main/persistance/JsonWriter.java
+++ src/main/persistance/JsonWriter.java
@@ -9,39 +9,40 @@ import java.io.*;
 // representation of Scrabble game to file
 // Citation: Based on WorkRoom example on edX
 public class JsonWriter {
-    
+    private static final int TAB = 4;
     private PrintWriter writer;
     private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer, throws FileNotFoundException
     // if destination file cannot be opened for writing
     public void open() throws FileNotFoundException {
-
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of 
     // Scrabble Game to file
     public void write(ScrabbleGame game) {
-
+        JSONObject json = game.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-
+        writer.print(json);
     }
 
 }
